d5fdca18b9ba011162dfe87a2724f9c99c22f4f7
juanDsalinas <juandsalinasnacun@gmail.com>
2023-08-20 16:36:19 -0500

    Update
    First Action

---
+++ Fundamentals/app.py
@@ -0,0 +1,29 @@
+import random
+# materias = input('ingrese cuantas materias vamos a programar \n')
+# materia = []
+# materia = materias.split(',')
+
+profes = [['vacio'],'tatiana','uldarico']
+
+profesTematicas = [['vacio'],['java','kotlin','proyecto'],['python','proyecto']]
+
+res=True
+
+horario = []
+for i in range(len(profes)):
+    print(f"el profesor {profes[i]} puede dictar {profesTematicas[i]}")
+    position = random.randint(1,3)
+    while res==True:
+        if len(profesTematicas) > 0:
+            print(f"{profes[position]} and {profesTematicas[position]}")
+        else : 
+            position = random.randint(1,3)
+            print(f"{profes[position]} and {profesTematicas[position]}")
+            
+
+
+# sede = []
+# salon = []
+
+# materia.pop(0)
+# print(materia)
